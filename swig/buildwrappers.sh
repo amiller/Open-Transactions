72f6db618124fd9b483e4aa2615c1656ff252290
Andrew Miller <amiller@dappervision.com>
2012-02-20 02:57:25 -0500

    Rewrote the buildwrappers.sh swig script to be more flexible, just like buildwrappers.bat
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- swig/buildwrappers.sh
+++ swig/buildwrappers.sh
@@ -1,46 +1,43 @@
-touch otapi.d; rm otapi.d;
-touch otapi.cs; rm otapi.cs;
-touch otapi.java; rm otapi.java;
-touch otapi.php; rm otapi.php;
-touch otapi.pm; rm otapi.pm;
-touch otapi.py; rm otapi.py;
-touch OTAPI.pyc; rm OTAPI.pyc;
-touch otapiJNI.java; rm otapiJNI.java;
-touch otapiPINVOKE.cs; rm otapiPINVOKE.cs;
-touch OTAPI_d.cpp; rm OTAPI_d.cpp;
-touch OTAPI_csharp.cpp; rm OTAPI_csharp.cpp;
-touch OTAPI_java.cpp; rm OTAPI_java.cpp;
-touch OTAPI_perl5.cpp; rm OTAPI_perl5.cpp;
-touch OTAPI_php.cpp; rm OTAPI_php.cpp;
-touch OTAPI_python.cpp; rm OTAPI_python.cpp;
-touch OTAPI_ruby.cpp; rm OTAPI_ruby.cpp;
-touch OTAPI_tcl.cpp; rm OTAPI_tcl.cpp;
-touch php_otapi.h; rm php_otapi.h;
-touch OTAPI_wrap.h; rm OTAPI_wrap.h;
-
-#Commenting out LISP since it doesn't seem to have std_string.i
-#touch otapi.lisp; rm otapi.lisp;
-#touch OTAPI_cffi.c; rm OTAPI_cffi.c;
-#touch OTAPI_wrap.c; rm OTAPI_wrap.c; /usr/bin/swig -c++ -cffi OTAPI.i; touch OTAPI_wrap.c; mv OTAPI_wrap.c OTAPI_cffi.c
-
-# Commenting out the D-language for now, since swig-d doesn't seem to be available on macports right now.
-#touch OTAPI_wrap.cxx; rm OTAPI_wrap.cxx; /usr/bin/swig -c++ -d -d2 OTAPI.i; touch OTAPI_wrap.cxx; mv OTAPI_wrap.cxx OTAPI_d.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_d.h
-
-touch OTAPI_wrap.cxx; rm OTAPI_wrap.cxx; swig -c++ -tcl OTAPI.i; touch OTAPI_wrap.cxx; mv OTAPI_wrap.cxx OTAPI_tcl.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_tcl.h
-touch OTAPI_wrap.cxx; rm OTAPI_wrap.cxx; swig -c++ -ruby OTAPI.i; touch OTAPI_wrap.cxx; mv OTAPI_wrap.cxx OTAPI_ruby.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_ruby.h
-touch OTAPI_wrap.cxx; rm OTAPI_wrap.cxx; swig -c++ -python OTAPI.i; touch OTAPI_wrap.cxx; mv OTAPI_wrap.cxx OTAPI_python.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_python.h
-touch OTAPI_wrap.cpp; rm OTAPI_wrap.cpp; swig -c++ -php OTAPI.i; touch OTAPI_wrap.cpp; mv OTAPI_wrap.cpp OTAPI_php.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_php.h
-touch OTAPI_wrap.cxx; rm OTAPI_wrap.cxx; swig -c++ -perl5 OTAPI.i; touch OTAPI_wrap.cxx; mv OTAPI_wrap.cxx OTAPI_perl5.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_perl5.h
-touch OTAPI_wrap.cxx; rm OTAPI_wrap.cxx; swig -c++ -csharp OTAPI.i; touch OTAPI_wrap.cxx; mv OTAPI_wrap.cxx OTAPI_csharp.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_csharp.h
-
-#I'm putting the Moneychanger package in here so I don't have to search/replace anymore.
-#touch OTAPI_wrap.cxx; rm OTAPI_wrap.cxx; /usr/bin/swig -c++ -java OTAPI.i; touch OTAPI_wrap.cxx; mv OTAPI_wrap.cxx OTAPI_java.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_java.h
-
-#PACKAGE VERSION (com.wrapper.core.jni)
-touch OTAPI_wrap.cxx; rm OTAPI_wrap.cxx; swig -c++ -java -package com.wrapper.core.jni OTAPI.i; touch OTAPI_wrap.cxx; mv OTAPI_wrap.cxx OTAPI_java.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_java.h
-
-#NO-PACKAGE VERSION
-#touch OTAPI_wrap.cxx; rm OTAPI_wrap.cxx; swig -c++ -java OTAPI.i; touch OTAPI_wrap.cxx; mv OTAPI_wrap.cxx OTAPI_java.cpp; touch OTAPI_wrap.h; mv OTAPI_wrap.h OTAPI_java.h
+#!/bin/bash
 
 
+# Check for swig
+if ! swig -version > /dev/null; then 
+    echo Cannot find swig...
+    exit 1
+fi
 
+
+for x in csharp java perl5 php python ruby tcl
+do
+    echo Generating for $x ...
+
+    rm -r glue/$x
+    mkdir glue/$x
+
+    # Remove existing temporary wrapper files
+    for ext in cxx cpp h; do
+	if [ -f otapi/OTAPI_wrap.$ext ]; then rm otapi/OTAPI_wrap.$ext; fi
+    done
+
+
+    if [ "$x" != "java" ]; then
+	echo swig -c++ -$x -outdir glue/$x otapi/otapi.i
+	swig -c++ -$x -outdir glue/$x otapi/otapi.i   
+    fi
+
+    if [ "$x" == "java" ]; then
+	echo swig -c++ -$x -package org.ot.wrapper.jni -outdir glue/$x otapi/otapi.i
+	swig -c++ -$x -package org.ot.wrapper.jni -outdir glue/$x otapi/otapi.i
+    fi
+
+
+    # Move and clean up wrapper files
+    for ext in cxx cpp h; do
+	if [ -f otapi/OTAPI_$x.$ext ]; then rm otapi/OTAPI_$x.$ext; fi
+	if [ -f otapi/OTAPI_wrap.$ext ]; then mv otapi/OTAPI_wrap.$ext otapi/OTAPI_$x.$ext; fi
+    done
+
+done
+
+echo Done!
